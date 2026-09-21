aa4a5ea4edb3dffa6447a7e83edf4e900f695258
mreb6832-cpu <mreb6832@gmail.com>
2026-09-21 21:11:00 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,39 @@
+import { initializeApp } from
+  "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import { getAuth } from
+  "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import { getFirestore } from
+  "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+import { getStorage } from
+  "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDNEAuCOWSbjtCXp00nN_8CnNCHOuBRH1g",
+  authDomain: "eb-marketiza.firebaseapp.com",
+  projectId: "eb-marketiza",
+  storageBucket: "eb-marketiza.firebasestorage.app",
+  messagingSenderId: "1081828417462",
+  appId: "1:1081828417462:web:1447a6f0b6fb4ed1b0eb17",
+  measurementId: "G-YH1VY3Y8VQ"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+const storage = getStorage(app);
+
+
+export {
+  app,
+  auth,
+  db,
+  storage
+};
